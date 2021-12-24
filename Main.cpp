4291115e9c5c906055cd28e6f0e5bb03533f7a41
BabushkaBoi01 <95980708+BabushkaBoi01@users.noreply.github.com>
2021-12-25 02:26:13 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+		int input=0;
+	cout<<"      CPU Scheduler Simulator           ";
+	cout<<"\n 1) Scheduling Method(None) ";
+	cout<<"\n 2) Preemptive Mode(Off) ";
+	cout<<"\n 3) Show Result ";
+	cout<<"\n 4) End Program ";
+	cout<<"\nOption >";
+  	cin>>input;
+ 		 if(input==1){
+			cout<<"\nChoose Scheculing Method to Implement";
+			cout<<"\n A) First Come First Served Scheduling";
+			cout<<"\n B) Shortest-job-First Scheduling";
+			cout<<"\n C) Priority Scheduling";
+			cout<<"\n D) Round-Robin Scheduling";
+}
+
+
+
+ 
+}
